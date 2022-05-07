a4db0f2fb924b038399459dcd1798cc8edfbc7ae
Aaron Noe <aarminoe@gmail.com>
2022-05-07 11:11:07 -0500

    commit tree 0d22dd322099e56e9538fe72880af10406fd75e3

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordArray = word.split('')
+  let copyArray = wordArray.slice()
+  let reversedArray = copyArray.reverse()
+  let joinReverse = reversedArray.join('')
+  return joinReverse === word
 }
+
 
 /* 
   Add your pseudocode here
+  check if any word representing word reversed, is spelled the same.
+  -split word up by letters and put those letters into an array
+  -make a shallow copy of that array
+  -use the reverse() function on the copied array
+  -join the reversed array and the original array back together into a 1 word string
+  -check if the reversed word is equal to the original word input
+  -it will return true or false
 */
 
 /*
@@ -20,6 +33,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log('')
+
+  console.log('Expecting: false')
+  console.log('=>', isPalindrome('algorithm'))
 }
 
 module.exports = isPalindrome;
